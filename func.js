c66a8418071a5976aa7ef7d85a1f7996aa326f9c
OmarObaid11 <>
2021-07-25 12:57:36 +0000

    func

---
+++ func.js
@@ -0,0 +1,27 @@
+
+
+
+const person = {
+  firstName: "Omar ",
+  lastName : "Obaid",
+  id       : 19012503,
+  location : "Amman",
+  sex : "Male",
+  age : "20"
+}
+console.log(person);
+
+
+function sum(a,b){
+  return a*b;
+
+
+}
+var result = sum(9,6);
+var result = result -6;
+var result = result *2;
+var result = result /3;
+
+
+console.log(result);
+
--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 var name = prompt("hello im omar :) what is you name")
 alert("hello welcome to my wibsite " + name )
 
-var firstname = "omar"
-console.log(name)
 
 
-var favfood = prompt("what is your fav food")
+
+var favfood = prompt("what is you fav Food? ,  shawirma, Pizza,burger")
+
+
+while(favfood != "shawirma" && favfood != "Pizza" && favfood != "burger"){
+
+var favfood = prompt("what is you fav Food? ,  shawirma, Pizza,burger")
+}
+
+var numPic = prompt("how many times you wish to eat it :P ")
+
+while(numPic > 7){
+numPic = prompt ("plz enter num less than 7!")
+}
 
 if(favfood == "shawirma"){
-  document.write("<div>" + "<h3>" + favfood + "</h3>" + "<img src=  'https://static.toiimg.com/thumb/64696930.cms?imgsize=329052&width=800&height=800'width='200' height='200' />" + "</div>")
+
+
+
+for(var i = 0; i< numPic; i++)
+{document.write(
+  "<div>" + "<h3>" + favfood + "</h3>" + "<img src=  'https://static.toiimg.com/thumb/64696930.cms?imgsize=329052&width=800&height=800'width='200' height='200' />" + "</div>")}
 }
+
 else if (favfood == "Pizza"){
-  document.write("<div>" + "<h3>" + favfood + "</h3>" + "<img src = 'https://alfaiomi.net/wp-content/uploads/2021/03/pizza.jpg 'width='200' height='200'/>" + "</div>")
-
+for(var i = 0; i< numPic;i++)
+{document.write("<div>" + "<h3>" + favfood + "</h3>" + "<img src = 'https://alfaiomi.net/wp-content/uploads/2021/03/pizza.jpg 'width='200' height='200'/>" + "</div>")}
 }
+
 else if  (favfood =="burger"){
-  document.write("<div>" + "<h3>" + favfood + "</h3>" + "<img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwR7X5iQN-smijnnIwobDvK2D8GeHLG8--wA&usqp=CAU 'width='200' height='200' />" + "</div>")
+for(var i = 0; i< numPic;i++)
+{document.write("<div>" + "<h3>" + favfood + "</h3>" + "<img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwR7X5iQN-smijnnIwobDvK2D8GeHLG8--wA&usqp=CAU 'width='200' height='200' />" + "</div>")}
+}
 
-}
-else {
-   alert("we dont have this car")
-}
 
 
 
